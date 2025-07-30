e0e7d85f190a80d8d37e1fcde0c209113932b35e
Cidree <adrian.cidre@gmail.com>
2025-07-30 17:52:14 +0200

    Internal function structure for predict funs

--- R/predict-height.R
+++ R/predict-height.R
@@ -1,25 +1,28 @@
 
 
-EquationHD <- S7::new_class(
-  name    = "EquationHD",
+ModelHD <- S7::new_class(
+  name    = "ModelHD",
   package = "silviculture",
   properties = list(
     equation = S7::new_property(S7::class_character, default = quote(list())),
+    species  = S7::new_property(S7::class_character, default = quote(list())),
+    doi      = S7::new_property(S7::class_character, default = quote(list())),
     params   = S7::new_property(S7::class_list, default = quote(list()))
   )
 )
 
 
 
+
+
 #' Estimates tree height from DBH
 #'
-#' This function estimates tree height using h-d equations.
+#' This function estimates tree height using h-d equations. Currently, only equations
+#' for Spain are available.
 #'
 #' @param diameter Numeric vector with diameters in cm
-#' @param species A character string specifying the scientific name of the tree
-#' species.
-#' @param equation A function. A function with the structure \code{eq_hd_*()} with
-#' additional arguments depending on the equation. Currently only [eq_hd_aitor2025] 
+#' @param model A function. A function with the structure \code{eq_hd_*()} with
+#' additional arguments depending on the model. Currently only [eq_hd_aitor2025()] 
 #' is available.
 #' @param quiet A logical value. If TRUE, suppresses any informational messages.
 #'
@@ -36,56 +39,49 @@ EquationHD <- S7::new_class(
 #' @examples
 #' 1 + 1 #TODO
 silv_predict_height <- function(diameter,
-                              species,
-                              equation,
-                              quiet = FALSE) {
+                                model,
+                                quiet = FALSE) {
   
   # 0. Handle errors
-  ## 0.2. Ensure species has same length as the rest, when only 1 species and several
-  ## diameters specified
-  if (length(species) == 1) species <- rep_len(species, length(diameter))
 
-  # 1. Apply equation
-  ## 1.1. Get equation params
-  params <- equation
-  ## 1.2. Apply the correct equation
-  if (params@equation == "aitor2025") {
-    # 2. Aitor et al. 2015
+  # 1. Apply selected model
+  if (model@equation == "aitor2025") {
+    ## 2. Aitor et al. 2025
     ## 2.1. Function to calculate the height rowwise (vectorized)
-    calc_height <- function(d, sp) {
-      ## 2.1. Get species params
-      selected_params <- h_d_aitor_tbl[tolower(h_d_aitor_tbl$species_name) == tolower(sp), ]
-      ## 2.2. If the species is not available...
-      if (nrow(selected_params) == 0) {
-        if (!quiet) cli::cli_warn("The species {sp} is not available. Using a generic equation.")
-        selected_params <- h_d_aitor_tbl[h_d_aitor_tbl$species_name == "All the species", ]
-      }
-      ## 2.3. Calculate total a and b
-      total_a <- selected_params$a + params@params$extra_a
-      total_b <- selected_params$b + params@params$extra_b
-      ## 2.4. Apply equation
+    calc_height <- function(d, sp) {      
+      ## 2.2. Select a and b based on species
+      selected_model <- model@params$species_params[model@params$species_params$species_name == sp, ]
+      total_a <- selected_model$a
+      total_b <- selected_model$b
+      ## 2.4. Apply model
       1.3 + total_a * (log(1 + d))**total_b
     }
 
     # 2. Vectorize the function to handle multiple rows
-    predicted_height <- mapply(calc_height, diameter, species)
+    predicted_height <- mapply(calc_height, diameter, model@species)
     
-    ## 2.5. Return reference 
-    if (!quiet) cli::cli_alert_warning("Cite this model using {.url #TODO}")
   }
 
+  ## 3. Return 
+  if (!quiet) cli::cli_alert_warning("Cite this model using {.url {model@doi}}")
   return(predicted_height)
 
 }
 
 
+
+
+
 #' Estimates tree height from DBH
 #'
-#' This function is made to be used in [silv_predict_height]. It implements the h-d equations
+#' This function is made to be used in [silv_predict_height()]. It implements the h-d equations
 #' developed in Aitor et al. (2025). These equations had been adjusted using the Spanish
 #' National Forest Inventory, and therefore, they should be used only within Spain. A wide
 #' range of tree species are available (90).
 #'
+#' @param species A character string specifying the scientific name of the tree
+#' species. It can be a column name if all the species are included in this model.
+#' See Details for available species.
 #' @param bioregion The biogreopgrahic region of the species. Available options are:
 #' \code{mediterranean}, \code{atlantic}, \code{alpine}, and \code{macaronesian}
 #' @param origin The origin of the stand. Available options are: \code{natural} and
@@ -97,18 +93,35 @@ silv_predict_height <- function(diameter,
 #' Details...#TODO
 #' 
 #' @references Reference #TODO
+#' 
+#' @seealso [silv_predict_height()]
 #'
 #' @return A numeric vector with predicted height
 #' @export
 #'
 #' @examples
 #' 1 + 1 #TODO
-eq_hd_aitor2025 <- function(bioregion = "mediterranean", 
+eq_hd_aitor2025 <- function(species, 
+                            bioregion = "mediterranean", 
                             origin    = "natural", 
                             mixture   = "pure") {
 
   # 1. Build parameters
-  ## 1.1. Bioregion
+  ## 1.1. Species
+  selected_params <- h_d_aitor_tbl[tolower(h_d_aitor_tbl$species_name) %in% tolower(species), ]
+  ## 1.2. Mange non-available species
+  na_species <- species[!species %in% selected_params$species_name]
+  if (length(na_species) > 0) {
+    cli::cli_warn("The species {na_species} is not available. Using a generic model.")
+    ## add generic model to the table
+    selected_params <- rbind(
+      selected_params, 
+      h_d_aitor_tbl[h_d_aitor_tbl$species_name == "All the species", ]
+    )
+    ## replace non-available species for "All the species"
+    species[!species %in% selected_params$species_name] <- "All the species"
+  }
+  ## 1.3. Bioregion
   bioregion_a <- switch(
     bioregion,
     "mediterranean" = 0,
@@ -124,7 +137,7 @@ eq_hd_aitor2025 <- function(bioregion = "mediterranean",
     "alpine"        = -0.0348,
     "macaronesian"  = -0.1995
   )
-  ## 1.2. Origin
+  ## 1.4. Origin
   origin_a <- switch(
     origin,
     "natural"    = 0,
@@ -136,7 +149,7 @@ eq_hd_aitor2025 <- function(bioregion = "mediterranean",
     "natural"    = 0,
     "plantation" = 0.0382
   )
-  ## 1.3. Mixture
+  ## 1.5. Mixture
   mixture_a <- switch(
     mixture,
     "pure" = 0,
@@ -148,16 +161,20 @@ eq_hd_aitor2025 <- function(bioregion = "mediterranean",
     "pure" = 0,
     "mix"  = -0.0390
   )
-  ## 1.4. Calculate extra a and b
+  ## 1.6. Calculate extra a and b
   extra_a <- mixture_a + origin_a + bioregion_a
   extra_b <- mixture_b + origin_b + bioregion_b
+  ## 1.7. Sum extra a and b
+  selected_params$a <- selected_params$a + extra_a
+  selected_params$b <- selected_params$b + extra_a
 
   # 2. Return
-  EquationHD(
+  ModelHD(
     equation = "aitor2025",
+    species  = species,
+    doi      = "Placeholder", #TODO
     params   = list(
-    extra_a = extra_a,
-    extra_b = extra_b
+      species_params = selected_params
     )
   )
 
