1b2b63d9dc710f57ec6ffca1cbc9c875f1b9d217
Cidree <adrian.cidre@gmail.com>
2026-03-29 11:22:04 +0300

    fix: :bug: Remove export from method

--- R/sample-size.R
+++ R/sample-size.R
@@ -516,7 +516,7 @@ plot <- S7::new_generic("plot", "x")
 #'
 #' @return A `ggplot` object representing the relationship between error and sample size.
 #'
-#' @export
+#' @name plot_SimpleSampleSize
 S7::method(plot, SimpleSampleSize) <- function(x, min_error = .01, max_error = .5) {
 
   ## create intervals
